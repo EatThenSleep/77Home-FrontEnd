f7990aa3a1b47e5479717f304bb88d5d712545bc
Long Nhat <nhathuynh227@gmail.com>
2024-11-27 16:43:32 +0700

    KAN-73 fix lỗi hiển thị modal xóa

--- src/components/owner/ListHouse.jsx
+++ src/components/owner/ListHouse.jsx
@@ -325,12 +325,7 @@ const ListHouse = () => {
       <Row className="mt-5">
         {currentItems && currentItems.length > 0 ? (
           currentItems.map((house, index) => (
-            <Col
-              md={12}
-              key={house.id}
-              className="mb-3"
-              onClick={() => handleViewDetail(house.id)}
-            >
+            <Col md={12} key={house.id} className="mb-3">
               <Card className="p-3">
                 <Row>
                   <Col md={4}>
@@ -339,6 +334,7 @@ const ListHouse = () => {
                       src={house.image}
                       alt={`Hình ảnh của ${house.name}`}
                       className="image-house"
+                      onClick={() => handleViewDetail(house.id)}
                     />
                   </Col>
                   <Col md={8} className="text-start">
