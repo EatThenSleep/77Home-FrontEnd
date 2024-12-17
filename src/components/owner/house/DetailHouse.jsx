2072d90971cf2de8947114d9bc0413bb20f3d55c
thanhlichqnuu <thanhlich2103gg@gmail.com>
2024-12-17 19:35:29 +0700

    Fix call api not work detail house

--- src/components/owner/house/DetailHouse.jsx
+++ src/components/owner/house/DetailHouse.jsx
@@ -1,137 +1,137 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
-import { useParams, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { Row, Col, Button, ListGroup, Card } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { FaImage } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 import "../../../styles/DetailHouse.scss";
+import { getHouseDetail } from "../../../service/houseService";
 
 const DetailHouse = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  const [detailHouse, setDetailHouse] = useState(null);
-  const [ownerName, setOwnerName] = useState("");
-  const [wardName, setWardName] = useState("");
+  const [detailHouse, setDetailHouse] = useState({});
 
   useEffect(() => {
     fetchHouseById();
-  }, [id]);
+  }, []);
 
   const fetchHouseById = async () => {
     try {
-      const res = await axios.get(`http://localhost:8080/api/v1/house/${id}`);
-      if (res && res.data) {
-        setDetailHouse(res.data);
-      }
-      // Lấy tên của chủ nhà
-      const ownerRes = await axios.get(`http://localhost:8080/api/v1/user/${res.data.DT.owner_id}`);
-      if (ownerRes && ownerRes.data) {
-        setOwnerName(ownerRes.data.fullname);
-      }
-
-      // Lấy tên của ward từ API
-      const wardRes = await axios.get(`http://localhost:8080/api/v1/ward/${res.data.DT.ward_id}`);
-      if (wardRes && wardRes.data) {
-        setWardName(wardRes.data.name); // Lưu tên ward vào state
+      const res = await getHouseDetail(id);
+      console.log("data", res.DT);
+      if (res && res.DT) {
+        setDetailHouse(res.DT);
       }
     } catch (error) {
       console.error("Error fetching house by ID:", error);
     }
   };
-
-  if (!detailHouse) {
-    return <div>Loading...</div>;
-  }
+  const statusMapping = {
+    1: "Đang hoạt động",
+    2: "Đang sửa chữa",
+    3: "Ngừng hoạt động",
+  };
+  const regionMapping = {
+    1: "Khu dân cư",
+    2: "Khu công nghiệp",
+    3: "Khu quy hoạch",
+  };
 
   return (
-    <Container className="p-4">
-      <Row className="p-3 mb-5 bg-white rounded shadow-sm">
-        {/* Hình ảnh */}
+    <Card className="">
+      <Row className="g-0">
         <Col
-          md={6}
-          className="d-flex justify-content-center align-items-center mb-4 mb-md-0"
+          md={4}
+          className="d-flex justify-content-center align-items-center px-2"
         >
-          {detailHouse.DT.avatar ? (
-            <img
-              src={detailHouse.DT.avatar}
-              className="image-house-detail img-fluid rounded"
-              alt={`Hình ảnh của ${detailHouse.DT.name}`}
+          {detailHouse.avatar ? (
+            <Card.Img
+              variant="top"
+              src={detailHouse.avatar}
+              alt="House avatar"
+              className="img-fluid rounded"
             />
           ) : (
-            <FaImage className="image-house-detail" size={200} />
+            <div className="d-flex justify-content-center align-items-center h-100">
+              <FaImage className="image-house-detail" />
+            </div>
           )}
         </Col>
-
-        {/* Chi tiết nhà */}
-        <Col md={6}>
-          <h3 className="mb-4">{detailHouse.DT.name || "Chưa cập nhật"}</h3>
-
-          <Row>
-            <Col xs={6} className="mb-2">
-              <strong>Địa chỉ:</strong> {detailHouse.DT.address || "N/A"}
-            </Col>
-            <Col xs={6} className="mb-2">
-              <strong>Diện tích:</strong> {detailHouse.DT.area || "N/A"} m²
-            </Col>
-            <Col xs={6} className="mb-2">
-              <strong>Số phòng:</strong> {detailHouse.DT.numberRooms || "N/A"}
-            </Col>
-            <Col xs={6} className="mb-2">
-              <strong>Số tầng:</strong> {detailHouse.DT.numberOfFloors || "N/A"}
-            </Col>
-            <Col xs={6} className="mb-2">
-              <strong>Năm xây dựng:</strong> {detailHouse.DT.yearBuilt || "N/A"}
-            </Col>
-            <Col xs={6} className="mb-2">
-              <strong>Vị trí:</strong> {detailHouse.DT.position || "N/A"}
-            </Col>
-            <Col xs={6} className="mb-2">
-              <strong>Kinh độ:</strong> {detailHouse.DT.longitude || "N/A"}
-            </Col>
-            <Col xs={6} className="mb-2">
-              <strong>Vĩ độ:</strong> {detailHouse.DT.latitude || "N/A"}
-            </Col>
-            <Col xs={6} className="mb-2">
-              <strong>Region:</strong> {
-                detailHouse.DT.region === 0 ? "Khu công nghiệp" :
-                  detailHouse.DT.region === 1 ? "Khu dân cư" :
-                    detailHouse.DT.region === 2 ? "Khu quy hoạch" :
-                      "N/A"
-              }
-            </Col>
-            <Col xs={6} className="mb-2">
-              <strong>Phường :</strong> {detailHouse.DT.ward_id || "N/A"}
-            </Col>
-            <Col xs={12} className="mb-2">
-              <strong>Mô tả:</strong> {detailHouse.DT.description || "Chưa có mô tả"}
-            </Col>
-            <Col xs={12} className="mb-3">
-              <strong>Trạng thái:</strong>{ 
-              detailHouse.DT.status === 1 ? "Còn trống" :
-                  detailHouse.DT.status === 2 ? "Đã thuê" :
-                    detailHouse.DT.status === 3 ? "Bảo trì" :
-                      "N/A"}
-            </Col>
-          </Row>
-
-          {/* Thông tin liên hệ */}
-          <div className="mb-3">
-            <strong>Thông tin liên lạc:</strong>
-            <p className="mb-1">
-              Tên chủ trọ : {detailHouse.DT.owner_id || "Chưa cập nhật"}
-            </p>
-            <p>Email: Chưa có dữ liệu</p>
-          </div>
-
-          {/* Nút Chỉnh sửa */}
-          <Button
-            variant="warning"
-            onClick={() => navigate("/house/update", { state: detailHouse })}
-          >
-            Chỉnh sửa
-          </Button>
+        <Col md={8}>
+          <Card.Body>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <strong>Tên:</strong> {detailHouse.name || "Không có dữ liệu"}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Địa chỉ:</strong> {detailHouse.address},
+                <span> phường {detailHouse?.ward?.name || ""}</span>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Năm xây dựng:</strong> {detailHouse.address},
+                {detailHouse.yearBuilt || "Không có dữ liệu"}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Mô tả:</strong>{" "}
+                {detailHouse.description || "Không có mô tả"}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Số tầng:</strong>{" "}
+                {detailHouse.numberOfFloors || "Không có dữ liệu"}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Số phòng:</strong>{" "}
+                {detailHouse.numberRooms || "Không có dữ liệu"}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Diện tích:</strong>{" "}
+                {detailHouse.area || "Không có dữ liệu"} m<sup>2</sup>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Trạng thái:</strong>{" "}
+                {statusMapping[detailHouse.status] || "Không có dữ liệu"}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Tọa độ:</strong> Kinh độ:
+                {detailHouse.longitude || "Không có dữ liệu"}, Vĩ độ:
+                {detailHouse.latitude || "Không có dữ liệu"}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Khu vực:</strong>{" "}
+                {regionMapping[detailHouse.region] || "Không có dữ liệu"}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Vị trí:</strong>{" "}
+                {detailHouse.position || "Không có dữ liệu"}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Chủ sở hữu:</strong>{" "}
+                {detailHouse.owner?.fullName || "Không có dữ liệu"}
+              </ListGroup.Item>
+            </ListGroup>
+            <div className="mt-4 mx-2">
+              <Button
+                variant="secondary"
+                className="mx-2"
+                onClick={() => navigate("/house")}
+              >
+                Quay lại
+              </Button>
+              <Button
+                variant="warning"
+                onClick={() =>
+                  navigate(`/house/update/${detailHouse.id}`, {
+                    state: detailHouse,
+                  })
+                }
+              >
+                Chỉnh sửa
+              </Button>
+            </div>
+          </Card.Body>
         </Col>
       </Row>
-    </Container>
+    </Card>
   );
 };
 
